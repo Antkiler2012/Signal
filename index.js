8fe525883219e5e1f9596e1387c2fbe19153dc24
Antoni <a.wrzesinski1@gmail.com>
2025-10-18 16:43:45 +0200

    added basic decode binary testing

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const commands = {
-  help: "help - List available commands\nabout - Information about the mission\nclear - Clear the terminal",
+  help: "help - List available commands\nabout - Information about the mission\nclear - Clear the terminal\nscan - Generates a new batch of signals and display their id and pattern.\ndecode_binary - decodes binary to text usage: decode_binary [binary]",
   about: "Your a cybersecurity agent getting a signals from a unknown source, your mission is to decipher the signals and handle them.",
+  scan: "",
+  decode_binary: ""
+};
+
+const binary = {
+  id03: "id03 = 01010011 01010010 01000011 00110011 01111100 01010100 00110010 01111100\n 01001100 00110101 01111100 01000110 00110000 01111100 01010000 01001000\n 01000101 01001100 01001100 01001111 01111100 01000011 00110111 00110100\n",
+  id07: "id07 = 01010011 01010010 01000011 00110111 01111100 01010100 00110001 01111100\n 01001100 00110000 01111100 01000110 00110000 01111100 01010000 01111100\n 01000011 00110000 00110000",
 };
 
 function promptline() {
@@ -24,6 +31,24 @@ function outputline(text) {
   return out;
 }
 
+function scan(container, inputParent) {
+  var i = Math.floor(Math.random() * 2) + 1;
+  if (i == 1) {
+      container.insertBefore(outputline(binary["id03"]), inputParent);
+  } else if (i == 2) {
+      container.insertBefore(outputline(binary["id07"]), inputParent);
+  }
+}
+
+function binaryAgent(str) {
+  var binString = '';
+  str.split(' ').map(function(bin) {
+      binString += String.fromCharCode(parseInt(bin, 2));
+   });
+   console.log(binString);
+   return binString;
+}
+
 function handle(command, container, inputParent) {
   if (command === "clear") {
     container.innerHTML = "";
@@ -32,6 +57,10 @@ function handle(command, container, inputParent) {
     newPrompt.querySelector("input").focus();
   } else if (commands[command]) {
     container.insertBefore(outputline(commands[command]), inputParent);
+  } else if (command === "scan") {
+    scan(container, inputParent);
+  } else if (command === "decode_binary") {
+    binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100');
   } else if (command !== "") {
     container.insertBefore(outputline(`Unknown command: ${command}`), inputParent);
   }
